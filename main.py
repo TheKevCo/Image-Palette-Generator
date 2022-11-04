58a051a22e079208c9e1c7f003d9437b4e39ea2f
Kevin Colon <koak1995@gmail.com>
2022-11-04 01:43:32 -0400

    Hexcode Functionality added.

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired
 from werkzeug.utils import secure_filename
 import os
+from io import BytesIO
+from PIL import Image
+from numpy import asarray
+import pandas as pd
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
@@ -28,12 +32,40 @@ class UploadForm(FlaskForm):
     file = FileField(validators=[FileRequired()])
 
 
+# https://www.youtube.com/watch?v=Ljo46Hsb-0Q
+def rgb_to_hex(rgb_list):
+    return '#' + '{:x}{:x}{:x}'.format(rgb_list[0], rgb_list[1], rgb_list[2])
+
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
     form = UploadForm()
     images = Img.query.all()
     image_data = images[0]
     image = b64encode(image_data.img).decode("utf-8")
+
+    # above is for displaying the image on the web page
+
+    #  -------------------------------------- code for making hexcode -------------------------------#
+    an_image = BytesIO(image_data.img)
+    the_img = Image.open(an_image)
+    hex_list = []
+    data = asarray(the_img)
+    for dimension in data:
+        for i in range(len(dimension)):
+            hex_list.append(rgb_to_hex(dimension[i]))
+    hex_dataframe = pd.DataFrame({'HexCodes': hex_list})
+    sum_hex = hex_dataframe.count()
+    top_10 = hex_dataframe['HexCodes'].value_counts().reset_index().head(10)
+    top_10['Percentage'] = (top_10['HexCodes'] / sum_hex.values) * 100
+    top_10_dict = top_10.to_dict()
+    top_10_hex = []
+    top_10_hexper = []
+    for i in range(10):
+        top_10_hex.append(top_10_dict['index'][i])
+        top_10_hexper.append(round(top_10_dict['Percentage'][i], 2))
+
+    #  -------------------------------------- code for making hexcode -------------------------------#
     if form.validate_on_submit():
         new_img = form.file.data
         filename = secure_filename(new_img.filename)
@@ -42,7 +74,8 @@ def home():
         db.session.add(input_img)
         db.session.commit()
         return redirect(url_for('home'))
-    return render_template('index.html', form=form, image_data=image_data, image=image)
+    return render_template('index.html', form=form, image_data=image_data, image=image, hexname=top_10_hex,
+                           hexper=top_10_hexper)
 
 
 @app.route("/true")
